bf8b927826e00a123e07b806864d2601dcd18332
Stanislaus Sunday <stanissandye@gmail.com>
2017-03-13 11:29:49 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ToDo List</title>
+    </head>
+    <body>
+        <h1>ToDo List</h1>
+        <ul>
+            <li>Taki</li>
+            <li>Mitsuha</li>
+            <li>Yotsuha</li>
+        </ul>
+        <form>
+            <input type="text" name="todo_item">
+            <button type="submit">Add</button>
+        </form>
+    </body>
+    
+</html>
